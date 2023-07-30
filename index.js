94d303825343c736d75d07f14350038e0e782221
Olayanju Oluwadamilare Samuel <hadreytechsolutions@gmail.com>
2023-07-30 22:10:45 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+  debugger;
+}
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+const writeCards = (["Ada", "Brendan", "Ali"], "birthday");
+function newWriteCards(writeCards){
+    for (let i = 0; i < writeCards.length; i++);
+
+    return (`Thank you, ${writeCards}, for the wonderful birthday gift!`)
+}
+
+
+let countDown = 0;
+while (countDown < 10) {
+  console.log(countDown++);
+}
+
+for (let countDown = 0; countDown < 10; countDown++) {
+    console.log(countDown);
+  }
